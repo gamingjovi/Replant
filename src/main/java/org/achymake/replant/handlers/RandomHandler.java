c28a63155b03a8e67fc87136e10c1ad4875c5b37
AchyMake <achymake@gmail.com>
2026-01-05 18:28:39 +0100

    RandomHandler updated

--- src/main/java/org/achymake/replant/handlers/RandomHandler.java
+++ src/main/java/org/achymake/replant/handlers/RandomHandler.java
@@ -7,16 +7,10 @@ public class RandomHandler {
         return new Random();
     }
     private double getRandomDouble() {
-        return 1.0 - getRandom().nextDouble(1.0);
-    }
-    private double getRandomDouble(double random) {
-        return getRandom().nextDouble(0.01, random);
+        return nextDouble(0, 1);
     }
     public boolean isTrue(double chance) {
         return chance >= getRandomDouble();
-    }
-    public boolean isTrue(double chance, double random) {
-        return chance >= getRandomDouble(random);
     }
     public double nextDouble(double origin, double bound) {
         return getRandom().nextDouble(origin, bound);
